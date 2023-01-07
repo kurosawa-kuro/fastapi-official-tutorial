a817afabf43580c4b50e3b76a9a744dda79b89a7
Toshifumi Kurosawa <Kuromailserver@gmail.com>
2023-01-08 01:50:52 +0900

    test_read_main

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,12 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app/sql_app.sqlite3"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:password@localhost:3306/training"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
---
+++ sql_app/test/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+
+from ..main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/users/")
+    print({"status_code": response.status_code,
+          "response": response.json()})
+    assert response.status_code == 200
+    assert response.json() == [{'email': 'string', 'id': 1, 'is_active': True, 'items': []},
+                               {'email': 'strissssssssng', 'id': 2, 'is_active': True, 'items': []}]
