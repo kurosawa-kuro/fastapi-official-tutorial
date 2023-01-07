813472b38aa344d91e4b0a49ecd9fa3d0ce816d9
Toshifumi Kurosawa <Kuromailserver@gmail.com>
2023-01-08 01:52:12 +0900

    c

--- sql_app/test/test_main.py
+++ sql_app/test/test_main.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/users/")
     print({"status_code": response.status_code,
-          "response": response.json()})
+          "response.json()": response.json()})
     assert response.status_code == 200
     assert response.json() == [{'email': 'string', 'id': 1, 'is_active': True, 'items': []},
                                {'email': 'strissssssssng', 'id': 2, 'is_active': True, 'items': []}]
